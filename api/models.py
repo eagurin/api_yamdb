f1ed1c738b09b60a48e7c6e47ff47fe52bc55c81
Алексей Кондратьев <andamak@yandex.ru>
2020-09-26 13:49:51 +0300

    add models rev and comm

--- api/models.py
+++ api/models.py
@@ -1,6 +1,8 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class Category(models.Model):
     name = models.CharField(max_length=150, verbose_name="Категория")
@@ -15,7 +17,7 @@ class Title(models.Model):
     )
     category = models.ForeignKey(Category, on_delete=models.SET_NULL,
                                  blank=True, null=True,
-                                 related_name='categories',
+                                 related_name="categories",
                                  verbose_name="Категория")
 
 
@@ -23,6 +25,32 @@ class Genre(models.Model):
     name = models.CharField(max_length=150, verbose_name="Жанр")
     slug = models.SlugField(max_length=50, verbose_name="slug", unique=True)
 
+
+class Reviews(models.model):
+    title = models.ForeignKey(Title, on_delete=models.CASCADE,
+        related_name="review_title")
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+        related_name="review_author")
+    text = models.TextField(verbose_name="Отзыв")
+    score = models.IntegerField(
+        validators =[MinValueValidator(1), MaxValueValidator(10)],
+        verbose_name="Оценка"
+    )
+    pub_date = models.DateTimeField(verbose_name="date published", 
+        auto_now_add=True)
+
+
+class Comments(models.model):
+    review = models.ForeignKey(Reviews, on_delete=models.CASCADE,
+        related_name="comment_review")
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+        related_name="comment_author") 
+    text = models.TextField(verbose_name="Комментарий")
+    pub_date = models.DateTimeField(verbose_name="date published", 
+        auto_now_add=True)    
+
+
+    
 
 
 
