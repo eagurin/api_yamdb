fbe9054c0dd395ebd751228cba1f7337b6d5bd82
kirflaer <kirflaer@gmail.com>
2020-09-27 13:30:38 +0700

    Поиск и пагинания для Категория жанров, урлы для удаления.

--- api/urls.py
+++ api/urls.py
@@ -9,11 +9,15 @@ router.register('titles', TitleViewSet, basename='Title')
 urlpatterns = [
     path('v1/', include(router.urls)),
     path('v1/genres/',
-         views.GenreViewSet.as_view(
-             {'get': 'list', 'post': 'create', 'delete': 'destroy'}),
+         views.GenreViewSet.as_view({'get': 'list', 'post': 'create'}),
          name="genres"),
-    path('v1/categories/',
-         views.CategoryViewSet.as_view(
-             {'get': 'list', 'post': 'create', 'delete': 'destroy'}),
+    path('v1/genres/<str:slug>/',
+         views.GenreViewSet.as_view({'delete': 'destroy'}),
+         name="genres_remove"),
+    path('v1/categories/', views.CategoryViewSet.as_view(
+        {'get': 'list', 'post': 'create'}),
          name="categories"),
+    path('v1/categories/<str:slug>/',
+         views.CategoryViewSet.as_view({'delete': 'destroy'}),
+         name="categories_remove"),
 ]
