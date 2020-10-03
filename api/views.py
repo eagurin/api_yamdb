fcb9fafd87f9a25e1ebd6b1039ff6460bc8dffe9
Алексей Кондратьев <andamak@yandex.ru>
2020-10-03 15:36:50 +0300

    begin

--- api/views.py
+++ api/views.py
@@ -65,7 +65,7 @@ class ReviewsViewSet(viewsets.ModelViewSet):
     pagination_class = PageNumberPagination
 
     def get_queryset(self):
-        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        title = get_object_or_404(Title, id=self.kwargs.get('id'))
         reviews = title.review_title.all()
         return reviews
 
