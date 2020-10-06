0f8b6bd47b18b5a6cfd589adefd0fe402d01994a
Алексей Кондратьев <andamak@yandex.ru>
2020-10-06 11:44:45 +0300

    заменил related name

--- api/views.py
+++ api/views.py
@@ -106,7 +106,7 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
-        comments = review.comment_review.all()
+        comments = review.comments.all()
         return comments
 
 
